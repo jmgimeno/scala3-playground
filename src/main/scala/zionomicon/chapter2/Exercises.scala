ad206c7a37c7a563d44bb21e78c8dc275213ffe7
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2021-06-21 20:09:16 +0200

    zionomicon: chapter 2 - exercise 2

--- src/main/scala/zionomicon/chapter2/Exercises.scala
+++ src/main/scala/zionomicon/chapter2/Exercises.scala
@@ -1,20 +1,34 @@
 package zionomicon.chapter2
 
-object Exercises {
+import java.io.PrintWriter
 
-  import zio.*
+import zio.*
 
-  // 1
+// 1
 
-  def readFile(file: String): String =
-    val source = scala.io.Source.fromFile(file)
-    try source.getLines().mkString finally source.close()
+def readFile(file: String): String =
+  val source = scala.io.Source.fromFile(file)
+  try source.getLines().mkString finally source.close()
 
-  def readFileZio(file: String): Task[String] =
-    ZIO.effect(readFile(file))
+def readFileZio(file: String): Task[String] =
+  ZIO.effect(readFile(file))
 
-  @main def exercise1: Unit =
-    val runtime = Runtime.default
-    val result = runtime.unsafeRun(readFileZio("build.sbt"))
-    print(result)
-}
+@main def exercise1: Unit =
+  val runtime = Runtime.default
+  val result = runtime.unsafeRun(readFileZio("build.sbt"))
+  print(result)
+
+// 2
+
+def writeFile(file: String, text: String): Unit =
+  import java.io.*
+  val pw = PrintWriter(File(file))
+  try pw.write(text) finally pw.close
+
+def writeFileZio(file: String, text: String): Task[Unit] =
+  ZIO.effect(writeFile(file, text))
+
+@main def exercise2: Unit =
+  val runtime = Runtime.default
+  val result = runtime.unsafeRun(writeFileZio("potato.txt", "potato"))
+
