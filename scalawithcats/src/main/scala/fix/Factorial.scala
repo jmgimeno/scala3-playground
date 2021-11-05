183e878a4f6e8455be3b807a12273d32b6e2e3cb
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2021-11-05 18:29:32 +0100

    Exploring Defer fix (now with simpler solution with Eval to compare to)

--- scalawithcats/src/main/scala/fix/Factorial.scala
+++ scalawithcats/src/main/scala/fix/Factorial.scala
@@ -9,6 +9,12 @@ object Factorial {
     if n == 0 then 1
     else n * fact(n - 1)
 
+  def factE(n: Int): Eval[BigInt] =
+    if (n == 0) then Eval.now(1)
+    else Eval.defer {
+      factE(n - 1).map(_ * n)
+    }
+
   def factF(recur: Int => Eval[BigInt]): Int => Eval[BigInt] = (n: Int) =>
     if n == 0 then Eval.now(1)
     else Eval.defer {
@@ -16,10 +22,12 @@ object Factorial {
     }
 
   // Stack-safe
-  val factFix = Defer[[A] =>> Function1[Int, A]].fix(factF)
+  val factFixF = Defer[[A] =>> Function1[Int, A]].fix(factF)
 
   def main(args: Array[String]): Unit = {
     //println(fact(10_000))
-    println(factFix(10_000).value)
+    println(factE(10_000).value)
+    println("-" * 30)
+    println(factFixF(10_000).value)
   }
 }
