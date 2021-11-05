e32f1f5f0b8be099a638e207b88577170e5a3e8b
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2021-11-05 12:44:19 +0100

    Exploring Defer fix (now stack-safe)

--- scalawithcats/src/main/scala/fix/Factorial.scala
+++ scalawithcats/src/main/scala/fix/Factorial.scala
@@ -5,19 +5,21 @@ import cats.Defer.*
 
 object Factorial {
 
-  def fact(n: Int): Int =
+  def fact(n: Int): BigInt =
     if n == 0 then 1
     else n * fact(n - 1)
 
-  def factF(recur: Int => Int) = (n: Int) =>
-    if n == 0 then 1
-    else n * recur(n - 1)
+  def factF(recur: Int => Eval[BigInt]): Int => Eval[BigInt] = (n: Int) =>
+    if n == 0 then Eval.now(1)
+    else Eval.defer {
+      recur(n - 1).map(_ * n)
+    }
 
-  // Not stack-safe
-  val factFix = Defer[[A] =>> Function1[Int, A]].fix[Int](factF)
+  // Stack-safe
+  val factFix = Defer[[A] =>> Function1[Int, A]].fix(factF)
 
   def main(args: Array[String]): Unit = {
-    println(fact(10))
-    println(factFix(10))
+    //println(fact(10_000))
+    println(factFix(10_000).value)
   }
 }
